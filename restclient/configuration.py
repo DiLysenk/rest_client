37b36bd73c50ec1e1c9046cb3e2dea9ee4db91dc
Dmitriy <dmlysenko@ptsecurity.com>
2025-04-27 22:12:34 +0300

    add rest cllient

---
+++ restclient/configuration.py
@@ -0,0 +1,13 @@
+from typing import Optional, Any
+
+
+class Configuration:
+    def __init__(
+        self,
+        host: str,
+        headers: Optional[dict[str, Any]] = None,
+        disable_log: bool = True,
+    ):
+        self.host = host
+        self.headers = headers
+        self.disable_log = disable_log
